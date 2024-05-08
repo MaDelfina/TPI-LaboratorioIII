521046e4a55c1752dad2fa1991c6eaf6bf7cf221
AnaRustici <anabellarustici@gmail.com>
2024-05-07 22:20:39 -0300

    add data

---
+++ tpi-laboratorioIII/src/components/data/Data.jsx
@@ -0,0 +1,16 @@
+export const CLIENTS = [
+    {id: 1, username: "pepe", password: "pepe123", shopping_cart: []},
+]
+
+export const ADMINS = [
+    {id: 1, username: "papa", password: "papa123"}, 
+    {id: 2, username: "ana", password: "ana123"}
+]
+export const SUPER_ADMINS = [
+    {id: 1, username: "Nicolas", password: "Nicolas123"}, 
+    {id: 2, username: "Anabella", password: "Anabella123"}, 
+    {id: 3, username: "Delfina", password: "Delfina123"}
+]
+export const PIZZAS = [
+    {id: 1, name: "papa", description: "", price: 0, stock: 0, imageUrl: ""}
+]
