0b23537f3639b86d561d567fcd53d7c26f6d4d69
N-Rem <nicolasromero.barrios@gmail.com>
2024-05-14 22:53:48 -0300

    Add List of product Items

--- tpi-laboratorioIII/src/App.jsx
+++ tpi-laboratorioIII/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ListProduct from './components/ListProduct/ListProduct'
 import Login from './components/login/Login'
 
 function App() {
   return (
     <>
       <Login></Login>
+      <ListProduct/>
     </>
   )
 }
