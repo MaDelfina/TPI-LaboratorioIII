0f42934f38ac98c3e3f23fb51308b49c52eb32ab
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-05-26 19:14:19 -0300

    create home

--- tpi-laboratorioIII/src/App.jsx
+++ tpi-laboratorioIII/src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
 
 import ListProduct from './components/ListProduct/ListProduct'
+import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
-
+import Search from "./components/search/SearchProduct"
 
 function App() {
   return (
     <>
-      <Login></Login>
-      <ListProduct/>
-      <Register/>
-
+      <Home/>
+     <ListProduct/>
     </>
   )
 }
--- tpi-laboratorioIII/src/components/ListProduct/ListProduct.jsx
+++ tpi-laboratorioIII/src/components/ListProduct/ListProduct.jsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import {PIZZAS, USERS} from '../data/Data';
-import ProductItem from '../productItem/ProductItem'
+import React, { useState } from 'react';
 import { Accordion, Container } from 'react-bootstrap';
+import { PIZZAS } from '../data/Data';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ProductItem from '../productItem/ProductItem';
+import SearchProduct from '../search/SearchProduct';
 import './listProduct.css';
 
-
-
 const ListProduct = () => {
+  const [pizzas, setPizzas] = useState(PIZZAS);
+
+  const searchHandler = (search) => {
+    const filterProducts = pizzas.filter(
+      (pizza) =>
+        pizza.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setPizzas(filterProducts);
+  
+    if (filterProducts.length > 0) {
+      console.log(filterProducts);
+    } else {
+      console.log("Pizza no encontrada");
+    }
+  };
+
   return (
-    <Container fluid='md' >
-      <Row>
-      <Col md={3}></Col>
+    <>
+      <SearchProduct onSearch={searchHandler} />
 
-        <Col md={6} className='Scroll-Bar' style={{ overflow: 'auto', width: '33rem', height: '35rem' }}>
-          < Accordion style={{ width: '30rem' }}>
-            {PIZZAS.map(pizza =>
-              <ProductItem
-                key={pizza.id}
-                name={pizza.name}
-                description={pizza.description}
-                price={pizza.price}
-                imgUrl={pizza.imageUrl}
-                id={pizza.id}
-              />)}
-          </Accordion >
-        </Col>
-        <Col md={3}></Col>
+      <Container fluid='md'>
+        <Row>
+          <Col md={3}></Col>
 
-      </Row>
-    </Container>
-  )
+          <Col md={6} className='Scroll-Bar' style={{ overflow: 'auto', width: '33rem', height: '35rem' }}>
+            <Accordion style={{ width: '30rem' }}>
+              {pizzas.map(pizza => (
+                <ProductItem
+                  key={pizza.id}
+                  name={pizza.name}
+                  description={pizza.description}
+                  price={pizza.price}
+                  imgUrl={pizza.imageUrl}
+                  id={pizza.id}
+                />
+              ))}
+            </Accordion>
+          </Col>
+          <Col md={3}></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default ListProduct
+export default ListProduct;
