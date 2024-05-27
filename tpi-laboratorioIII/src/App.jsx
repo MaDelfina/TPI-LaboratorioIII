16cc403bfd817e7a2a084d7fb153075367245138
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-05-27 15:30:58 -0300

    arreglos en home

--- tpi-laboratorioIII/src/App.jsx
+++ tpi-laboratorioIII/src/App.jsx
@@ -2,9 +2,6 @@ import './App.css'
 
 import ListProduct from './components/ListProduct/ListProduct'
 import Home from './components/home/Home'
-import Login from './components/login/Login'
-import Register from './components/register/Register'
-import Search from "./components/search/SearchProduct"
 
 function App() {
   return (
