cc65ce2367b1a5f9ad8b9310c6422780d696b1f1
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-06-13 12:49:44 -0300

    delete button

--- tpi-laboratorioIII/src/components/eachClient/EachClient.jsx
+++ tpi-laboratorioIII/src/components/eachClient/EachClient.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { Button, ListGroup } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
-const EachClient = ({ name }) => {
+const EachClient = ({ id, name, onDelete }) => {
     return (
         <ListGroup.Item variant='dark' className="d-flex align-items-center justify-content-between">
             {name}
-            <Button variant='danger'>Delete client</Button>
+            <Button variant='danger' onClick={() => onDelete(id)} >Delete client</Button>
         </ListGroup.Item>
     )
 }
 
 EachClient.propType = {
-    name: PropTypes.string
+    id: PropTypes.number,
+    name: PropTypes.string,
+    onDelete: PropTypes.func
 }
 
 export default EachClient
